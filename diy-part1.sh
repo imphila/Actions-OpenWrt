1ef6446636a1cc8402e9d1370bf6fe6688f2726c
philia <imphila@users.noreply.github.com>
2020-08-01 21:44:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,6 @@
 
 #!/bin/bash
 
-git clone https://github.com/maxlicheng/luci-app-ssr-plus.git package/luci-app-ssr-plus
 mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
